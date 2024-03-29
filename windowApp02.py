03cf3ff067179e86aaeffc16461b6e85aeb58192
sseungho <bedder9antwerp@hotmail.com>
2024-03-29 15:24:23 +0900

    2nd

--- windowApp02.py
+++ windowApp02.py
@@ -10,12 +10,13 @@ class MyWindow(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)  # 위에서 불러온 test.ui 연결
-        self.setWindowTitle("It's Friday!")
+        self.setWindowTitle("금요일 GUI")
+        self.setWindowIcon(QIcon("IMG/Image20240326124632.png"))
         self.bttn01.clicked.connect(self.bttn01_click)
         # 버튼1이 클릭 >>> bttn01_click 메써드 호출
         self.bttn02.clicked.connect(self.bttn02_click)
         # 버튼1이 클릭 >>> bttn01_click 메써드 호출
-
+        self.statusBar().showMessage("This is my truth tell me yours")
     def bttn01_click(self):
         self.label1.setText('Hello World~~~')
 
